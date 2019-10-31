76dbf0819d0cfa62b714854eefd2a60db84be613
zero <vipgago9x@gmail.com>
2019-10-31 21:29:44 +0700

    delete all

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-/* GET home page. */
-app.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-module.exports = app;
